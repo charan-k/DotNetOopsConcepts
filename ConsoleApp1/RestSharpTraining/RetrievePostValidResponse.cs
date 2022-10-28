87d13dc6db940df38247a6620a22104893f6237f
Charan Kumar <Charan_Kumar@epam.com>
2022-10-28 20:42:03 +0530

    updated json

--- ConsoleApp1/RestSharpTraining/RetrievePostValidResponse.cs
+++ ConsoleApp1/RestSharpTraining/RetrievePostValidResponse.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,7 +9,8 @@ namespace RestSharpTraining
 {
     class RetrievePostValidResponse
     {
-        public int id { get; set; }
+        [JsonProperty("id")]
+        public int Inte { get; set; }
 
         public string? title { get; set; }
 
--- ConsoleApp1/RestSharpTraining/UnitTest1.cs
+++ ConsoleApp1/RestSharpTraining/UnitTest1.cs
@@ -1,5 +1,6 @@
 using Newtonsoft.Json;
 using RestSharp;
+using RestSharp.Serialization.Json;
 using System.Net;
 
 namespace RestSharpTraining
@@ -11,21 +12,68 @@ namespace RestSharpTraining
         {
         }
 
+       
+        [TestCase("nl", "3825", HttpStatusCode.OK, TestName = "Check status code for NL zip code 7411")]
+        [TestCase("lv", "1050", HttpStatusCode.NotFound, TestName = "Check status code for LV zip code 1050")]
+        public void StatusCodeTest(string countryCode, string zipCode, HttpStatusCode expectedHttpStatusCode)
+        {
+            // arrange
+            RestClient client = new RestClient("http://api.zippopotam.us");
+            RestRequest request = new RestRequest($"{countryCode}/{zipCode}", Method.GET);
+
+            // act
+            IRestResponse response = client.Execute(request);
+
+            // assert
+            Assert.That(response.StatusCode, Is.EqualTo(expectedHttpStatusCode));
+        }
+
+        [TestCase("posts", "2", HttpStatusCode.OK, TestName = "Check Post status code for 4")]
+        [TestCase("posts", "7", HttpStatusCode.NotFound, TestName = "Check Post with not found status code")]
+        public void RetrievePosts(string posts,string id,HttpStatusCode httpStatusCode)
+        {
+            RestClient client = new RestClient("http://localhost:3000");
+            RestRequest req = new RestRequest($"{posts}/{id}", Method.GET);
+
+        //    req.AddQueryParameter("id", "2");
+        //    req.AddHeader("Accept", "application/json");
+
+            IRestResponse rest = client.Execute(req);
+            Assert.AreEqual(httpStatusCode, rest.StatusCode);
+            string response = rest.Content;
+
+            var responseinCorrectFormat = JsonConvert.DeserializeObject<RetrievePostValidResponse>
+                (response);
+
+            RetrievePostValidResponse locationResponse =
+        new JsonDeserializer().Deserialize<RetrievePostValidResponse>(rest);
+
+            Assert.AreEqual("origin of species", responseinCorrectFormat.title);
+            Assert.AreEqual("application/json; charset=utf-8", rest.ContentType);
+
+        }
+
         [Test]
         public void RetrievePosts()
         {
             RestClient client = new RestClient("http://localhost:3000");
-            RestRequest req = new RestRequest("posts", Method.GET);
-            req.AddQueryParameter("id", "1");
-            req.AddHeader("Accept", "application/json");
+            RestRequest req = new RestRequest("posts/2", Method.GET);
+
+            //    req.AddQueryParameter("id", "2");
+            //    req.AddHeader("Accept", "application/json");
+
             IRestResponse rest = client.Execute(req);
             Assert.AreEqual(HttpStatusCode.OK, rest.StatusCode);
             string response = rest.Content;
 
-            var responseinCorrectFormat = JsonConvert.DeserializeObject<RetrievePostValidResponse[]>
+            var responseinCorrectFormat = JsonConvert.DeserializeObject<RetrievePostValidResponse>
                 (response);
 
-            Assert.AreEqual("json-server", responseinCorrectFormat[0].title);
+            RetrievePostValidResponse locationResponse =
+        new JsonDeserializer().Deserialize<RetrievePostValidResponse>(rest);
+
+            Assert.AreEqual("origin of species", responseinCorrectFormat.title);
+            Assert.AreEqual("application/json; charset=utf-8", rest.ContentType);
 
         }
 
@@ -33,13 +81,13 @@ namespace RestSharpTraining
         public void UpdatePost()
         {
             RestClient client = new RestClient("http://localhost:3000");
-            RestRequest req = new RestRequest("posts/3", Method.PUT);
-            req.AddQueryParameter("id", "4");
+            RestRequest req = new RestRequest("posts/4", Method.PUT);
+         //   req.AddQueryParameter("id", "4");
 
 
             req.AddHeader("Accept", "application/json");
 
-            string body = CreatePostRequestbody(3, "title03", "author03");
+            string body = CreatePostRequestbody(4, "title03", "author03");
             req.AddJsonBody(body);
 
             IRestResponse rest = client.Execute(req);
@@ -59,10 +107,10 @@ namespace RestSharpTraining
         public void DeletePost()
         {
             RestClient client = new RestClient("http://localhost:3000");
-            RestRequest req = new RestRequest("posts/3", Method.DELETE);
-            req.AddQueryParameter("id", "4");
+            RestRequest req = new RestRequest("posts/4", Method.DELETE);
+         //   req.AddQueryParameter("id", "4");
 
-
+        
             req.AddHeader("Accept", "application/json");
 
            IRestResponse rest = client.Execute(req);
@@ -76,12 +124,11 @@ namespace RestSharpTraining
         {
             RestClient client = new RestClient("http://localhost:3000");
             RestRequest req = new RestRequest("posts", Method.POST);
-            req.AddQueryParameter("id", "4");
 
 
             req.AddHeader("Accept", "application/json");
 
-            string body = CreatePostRequestbody(4, "harry potter and goblet", "Rowling");
+            string body = CreatePostRequestbody(4, "autobiography", "nirad chowdary");
             req.AddJsonBody(body);
 
             IRestResponse rest = client.Execute(req);
@@ -92,9 +139,8 @@ namespace RestSharpTraining
             var responseinCorrectFormat = JsonConvert.DeserializeObject<CreatePostValidResponse>
                 (response);
 
-            Assert.AreEqual("harry potter and goblet", responseinCorrectFormat.title);
-            Assert.AreEqual("Rowling", responseinCorrectFormat.author);
-
+            Assert.AreEqual("autobiography", responseinCorrectFormat.title);
+            Assert.AreEqual("nirad chowdary", responseinCorrectFormat.author);
         }
 
         private string CreatePostRequestbody(int id,string title,string author)
